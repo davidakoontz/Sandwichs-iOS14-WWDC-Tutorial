f84de73dd13c3f1faa203cf9fdf2de0880384dd5
davidakoontz <david@koontz.name>
2020-06-24 21:16:10 -0500

    Initial Commit

---
+++ Shared/SandwichsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SandwichsApp.swift
+//  Shared
+//
+//  Created by David on 6/24/20.
+//
+
+import SwiftUI
+
+@main
+struct SandwichsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
