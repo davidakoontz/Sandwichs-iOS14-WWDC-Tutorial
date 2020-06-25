0f37e8d502e15b4116373ffeed45e706c8a86acd
davidakoontz <david@koontz.name>
2020-06-24 22:59:36 -0500

    Added new Swift View file SandwichDetail ... next hook it up to the NavigationView control

---
+++ Shared/SandwichDetail.swift
@@ -0,0 +1,20 @@
+//
+//  SandwichDetail.swift
+//  Sandwichs
+//
+//  Created by David on 6/24/20.
+//
+
+import SwiftUI
+
+struct SandwichDetail: View {
+    var body: some View {
+        Text("Sandwich Detail Page")
+    }
+}
+
+struct SandwichDetail_Previews: PreviewProvider {
+    static var previews: some View {
+        SandwichDetail()
+    }
+}
