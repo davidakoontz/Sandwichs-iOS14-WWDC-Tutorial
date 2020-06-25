898d88ff269f06bf6d92fbfd5c7af342d6cbbca3
davidakoontz <david@koontz.name>
2020-06-25 16:18:17 -0500

    Added more sandwich data to Sandwich.swift data file.  Note the image for "ice cream sandwich" is misspelled with a lower case "i" which causes it to be not found - change the spelling of the image file name.

--- Shared/Sandwich.swift
+++ Shared/Sandwich.swift
@@ -19,5 +19,13 @@ struct Sandwich: Identifiable {
 
 let testData = [
     Sandwich(name: "Club", ingredientCount: 4, isSpicy: false),
-    Sandwich(name: "Hot dog", ingredientCount: 5, isSpicy: true)
+    Sandwich(name: "Hot dog", ingredientCount: 5, isSpicy: true),
+    Sandwich(name: "Pastrami on rye", ingredientCount: 4, isSpicy: true),
+    Sandwich(name: "French dip", ingredientCount: 3, isSpicy: false),
+    Sandwich(name: "Banh mi", ingredientCount: 5, isSpicy: true),
+    Sandwich(name: "Ice cream sandwich", ingredientCount: 2, isSpicy: false),
+    Sandwich(name: "Croque madame", ingredientCount: 4, isSpicy: false),
+    Sandwich(name: "Fluffernutter", ingredientCount: 2, isSpicy: false),
+    Sandwich(name: "Avocado toast", ingredientCount: 3, isSpicy: true),
+    Sandwich(name: "Gua bao", ingredientCount: 4, isSpicy: true),
 ]
